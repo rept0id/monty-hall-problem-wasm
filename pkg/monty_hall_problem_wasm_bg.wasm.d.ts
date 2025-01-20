aa6cb8dde564d0c44df26c20854b281e3c3ec52a
Rantouan Achmet <rad@simplecode.gr>
2025-01-20 16:27:47 +0200

    ...

--- pkg/monty_hall_problem_wasm_bg.wasm.d.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export const monty_hall_problem_wasm: (a: number) => void;
-export const __wbindgen_exn_store: (a: number) => void;
-export const __wbindgen_add_to_stack_pointer: (a: number) => number;
-export const __wbindgen_free: (a: number, b: number, c: number) => void;
