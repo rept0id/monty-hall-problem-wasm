63f7785e99a9d6fdf7c3fe86203231b3270647ff
Rantouan Achmet <rad@simplecode.gr>
2025-01-21 11:21:36 +0200

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use serde_wasm_bindgen::to_value;
 
 /*** * * ***/
 
-pub mod model;
+mod model;
 
 /*** * * ***/
 
@@ -18,8 +18,7 @@ const DEF_CURTAINS: i32 = 3;
 
 /*** * * ***/
 
-fn init_states_simulation(simulation: &mut crate::model::model::Simulation) {
-    // Iterate over the states vector by index
+fn set_states_cases(simulation: &mut crate::model::model::Simulation) {
     for (i, state) in simulation.states.iter_mut().enumerate() {
         match i {
             0 => {
@@ -43,7 +42,7 @@ fn init_states_simulation(simulation: &mut crate::model::model::Simulation) {
     }
 }
 
-pub fn simulate(simulation: &mut crate::model::model::Simulation) {
+fn simulate(simulation: &mut crate::model::model::Simulation) {
     let mut rng = rand::thread_rng();
 
     for _ in 0..DEF_STATE_GAMES {
@@ -55,21 +54,96 @@ pub fn simulate(simulation: &mut crate::model::model::Simulation) {
             };
 
             if simulation.states[s].do_host_reveal {
-                loop {
-                    game.host_curtain_idx = rng.gen_range(0..DEF_CURTAINS);
-                    if game.host_curtain_idx != game.win_curtain_idx {
+                // if curtains are 3, there is only one curtain to switch to,
+                //  as the other is either the winning or the player choosen
+                //  thus, we want a deterministic approach.
+                // else, we want a purely random approach (and not one that just picks the next).
+                if DEF_CURTAINS == 3 {
+                    for i in 0..3 {
+                        if i == game.win_curtain_idx {
+                            continue;
+                        }
+
+                        if i == game.player_curtain_idx {
+                            continue;
+                        }
+
+                        /*** * * ***/
+
+                        game.host_curtain_idx = i;
+                        break;
+                    }
+                } else {
+                    loop {
+                        let n: i32;
+
+                        /*** * * ***/
+
+                        n = rng.gen_range(0..DEF_CURTAINS);
+
+                        /*** * * ***/
+
+                        if n == game.win_curtain_idx {
+                            continue;
+                        }
+
+                        if n == game.player_curtain_idx {
+                            continue;
+                        };
+
+                        /*** * * ***/
+
+                        game.host_curtain_idx = n;
                         break;
                     }
                 }
             }
 
             if simulation.states[s].do_player_change {
-                loop {
-                    let new_player_curtain_idx = rng.gen_range(0..DEF_CURTAINS);
-                    if new_player_curtain_idx != game.player_curtain_idx
-                        && new_player_curtain_idx != game.host_curtain_idx
-                    {
-                        game.player_curtain_idx = new_player_curtain_idx;
+                // if curtains are 3, there is only one curtain to switch to,
+                //  as the other is either the winning or the host choosen
+                //  thus, we want a deterministic approach.
+                // else, we want a purely random approach (and not one that just picks the next).
+                if DEF_CURTAINS == 3 {
+                    for i in 0..3 {
+                        if i == game.player_curtain_idx {
+                            continue;
+                        }
+
+                        if simulation.states[s].do_host_reveal {
+                            if i == game.host_curtain_idx {
+                                continue;
+                            }
+                        }
+
+                        /*** * * ***/
+
+                        game.player_curtain_idx = i;
+                        break;
+                    }
+                } else {
+                    loop {
+                        let n: i32;
+
+                        /*** * * ***/
+
+                        n = rng.gen_range(0..DEF_CURTAINS);
+
+                        /*** * * ***/
+
+                        if n == game.player_curtain_idx {
+                            continue;
+                        }
+
+                        if simulation.states[s].do_host_reveal {
+                            if n == game.host_curtain_idx {
+                                continue;
+                            }
+                        }
+
+                        /*** * * ***/
+
+                        game.player_curtain_idx = n;
                         break;
                     }
                 }
@@ -88,7 +162,7 @@ pub fn simulate(simulation: &mut crate::model::model::Simulation) {
 pub fn monty_hall_problem_wasm() -> wasm_bindgen::JsValue {
     let mut simulation = crate::model::model::Simulation::new(STATES as usize);
 
-    init_states_simulation(&mut simulation);
+    set_states_cases(&mut simulation);
     simulate(&mut simulation);
 
     to_value(&simulation).unwrap()
